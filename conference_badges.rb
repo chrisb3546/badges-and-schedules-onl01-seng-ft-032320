8b9872d9e0f3156ddca122afbd45504cf3c399c7
chris brady <chrisb3546@yahoo.com>
2020-03-27 14:32:55 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -7,10 +7,3 @@ def badge_maker("Arel")
   return "Hello, my name is #{"Arel"}."
 end
 
-def batch_badge_creator(names)
- return "Hello, my name is #{names}."
-end
-
-
-
- batch_badge_creator(names)
