6edcf60389efce752d8de72b3715b96e7e01ef6a
chris brady <chrisb3546@yahoo.com>
2020-03-27 14:25:17 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,17 @@
 # Write your code here.
+
+names = ["Edsger", "Ada", "Charles", "Alan",
+"Grace", "Linus","Matz"]
+
+def badge_maker("Arel")
+  return "Hello, my name is #{"Arel"}."
+end
+
+def batch_badge_creator(names)
+  names. each do |name|
+  "Hello, my name is #{name}.")
+  end
+  return conference_badges
+end
+
+
