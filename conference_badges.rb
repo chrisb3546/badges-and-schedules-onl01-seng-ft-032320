e47f322745d1011d6fd825532a558850530ef398
chris brady <chrisb3546@yahoo.com>
2020-03-27 16:25:28 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -26,7 +26,7 @@ end
 def printer(attendees)
  badges = batch_badge_creator(attendees)
  rooms = assign_rooms(attendees)
- puts attendees.each do |badges|
+ puts attendees.map do |badges|
 end
 end
 
